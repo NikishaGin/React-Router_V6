54778d4dbd6ead96ae31316c132e69655cb7e816
Nikita Sedov <nikishagin@gmail.com>
2024-07-24 17:55:46 +0300

    create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <header>
+                <a href={'/'}>Home</a>
+                <a href={'/posts'}>Blog</a>
+                <a href={'/about'}>About</a>
+            </header>
+            <div>
+                React-Router-dom_v6
+            </div>
+        </>
+    );
 }
 
 export default App;
